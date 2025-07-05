0d2fddce419defdc782d05130cb8709a799331a0
Hoang Gia Han Nguyen <nhgh250305@gmail.com>
2025-07-05 19:40:23 +0700

    loyaltyCard complete

--- app/src/main/java/com/example/thecodecup/ui/screens/RewardsScreen.kt
+++ app/src/main/java/com/example/thecodecup/ui/screens/RewardsScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -163,6 +164,29 @@ fun RewardsScreen(
                                         modifier = Modifier.size(16.dp)
                                     )
                                 }
+                            }
+                        }
+
+                        if (currentStamps >= maxStamps) {
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            Button(
+                                onClick = {
+                                    loyaltyViewModel.redeemFreeCard()
+                                },
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.White
+                                ),
+                                shape = RoundedCornerShape(12.dp)
+                            ) {
+                                Text(
+                                    text = "Redeem Free Coffee",
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color(0xFF4CAF50),
+                                    modifier = Modifier.padding(vertical = 4.dp)
+                                )
                             }
                         }
                     }
